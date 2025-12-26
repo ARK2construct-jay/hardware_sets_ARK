30cc2ac09ecc081a6c831a37c27454fa856aba4f
Jayesh Paliwal <jayeshpaliwal646@gmail.com>
2025-12-26 12:06:16 +0530

    Update to correct backend URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  API_BASE_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://hardware-sets-ark-backend.onrender.com'
-    : 'http://localhost:5000'
+  API_BASE_URL: 'https://hardware-sets-ark-backend.onrender.com'
 };
 
 export default config;
